579e71fbcb2a93e8e26e1f0e4201cb60920e6cb2
aay18-aar12 <133048419+aay18-aar12@users.noreply.github.com>
2024-02-12 22:04:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/all')
 def hello_world():
 
-    request2 = requests.get('https://maps2.dcgis.dc.gov/dcgis/rest/services/DCGIS_DATA/Health_WebMercator/MapServer/32/query?where=1%3D1&outFields=*&outSR=4326&f=json')
+    request2 = requests.get('https://maps2.dcgis.dc.gov/dcgis/rest/services/DCGIS_DATA/Health_WebMercator/MapServer/32/query?where=1%3D1&outFields=ID2,NAME,ID,TOTALPOP,P_POC,P_CHILD,P_ELDERLY,P_POVERTY,P_DISABILITY,P_LIMENG,ASTHMA,CHD,OBESITY,HSI,P_TREECOVER,P_NOTREE,P_IMPSURF,AIRTEMP_MEAN,HEI,HSEI,CREATED,EDITED&returnGeometry=false&outSR=4326&f=json')
 
     return request2.json()
 
